c2d3a68c9f39e7b8ffbe50d8ac4bb54fd8993fd8
minjibot <dqsjmj2@gmail.com>
2023-06-20 00:10:12 +0900

    최신 소스 받아 충돌만 머지 후 커밋

--- src/main/java/com/study/figure/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/study/figure/service/impl/ProjectServiceImpl.java
@@ -1,22 +1,44 @@
 package com.study.figure.service.impl;
 
-import java.util.List;
-
+import com.study.figure.mybatis.ProjectMapper;
+import com.study.figure.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.study.figure.dto.Project;
-import com.study.figure.mybatis.ProjectMapper;
-import com.study.figure.service.ProjectService;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
-public class ProjectServiceImpl implements ProjectService{
+public class ProjectServiceImpl implements ProjectService {
     
     @Autowired
-    ProjectMapper projectMapper;
+    private ProjectMapper projectMapper;
+
+    public List<Project> getProjects(Long userId) throws Exception {
+        return projectMapper.getProjects(userId);
+    }
 
     public List<Project> getUserProjects(Long userId) throws Exception {
         return projectMapper.getUserProjects(userId);
     }
-}
 
+    public int saveBookmark(Map<String, Object> data) throws Exception {
+        if (data == null) return 0;
+        int result = 0;
+
+        int cnt = projectMapper.getBookmarkCount(data);
+
+        if (cnt > 0) 
+            result = projectMapper.deleteBookmark(data);
+        else 
+            result = projectMapper.addBookmark(data);
+        
+        return result;
+    }
+
+    public List<Map<String, Object>> getProjectUsers(Map<String, Object> data) throws Exception {
+        return projectMapper.getProjectUsers(data);
+    }
+}                 
